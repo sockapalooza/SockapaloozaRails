bdf25787d4277171648612e88253ad1cc6862c6b
rittersm <rittersm@gmail.com>
2016-08-09 15:03:20 -0400

    current_order and current_user added

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,21 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+  helper_method :current_order
+  helper_method :current_user
+
+  def current_order
+    if !session[:order_id].nil?
+      Order.find(session[:order_id])
+    else
+      Order.new
+    end
+  end
+
+  def current_user
+    if session[:username]
+     @current_user ||= User.find_by(username: session[:username])
+    end
+    @current_user
+  end
+
 end
