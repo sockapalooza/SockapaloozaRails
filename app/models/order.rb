52102972b8edf5363815fe2a7555fa1c6d02f971
rittersm <rittersm@gmail.com>
2016-08-09 13:44:49 -0400

    order model cart functionality

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,5 +1,21 @@
 class Order < ApplicationRecord
-  has_many :products through :orderings
+  belongs_to :order_status
   has_many :orderings
+  before_create :set_order_status
+  before_save :update_subtotal
+
+  def subtotal
+    orderings.collect{|orderings| oi.valid? ? (orderings.quantity * orderings.unit_price):0}.sum
+  end
+
+  private
+
+  def set_order_status
+    self.order_status_id = 1
+  end
+
+  def update_subtotal
+    self[:subtotal] = subtotal
+  end
 
 end
