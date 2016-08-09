a830dfc6e3b4ef3d9ee6385747c7f87453e49fa7
rittersm <rittersm@gmail.com>
2016-08-09 14:20:56 -0400

    model changes complete for cart

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160809175224) do
+ActiveRecord::Schema.define(version: 20160809175913) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "order_statuses", force: :cascade do |t|
+    t.string   "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "orderings", force: :cascade do |t|
     t.integer  "product_id"
