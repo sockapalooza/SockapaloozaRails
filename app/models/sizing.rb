f23b92ece2cf919c9802e78ec223038772a6aa0c
dwegrzen <dwegrzen@gmail.com>
2016-08-12 14:24:28 -0400

    sizing model adjust

--- app/models/sizing.rb
+++ app/models/sizing.rb
@@ -3,7 +3,7 @@ class Sizing < ApplicationRecord
   belongs_to :product
 
   def self.checkstock(size_id, product_id, orderamount)
-    Sizing.where(product_id: product_id).where(size_id: size_id).first.quantity >= orderamount
+    Sizing.where(product_id: product_id)&.where(size_id: size_id).first.quantity >= orderamount
   end
 
   def admin_display
